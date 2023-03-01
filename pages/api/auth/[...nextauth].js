67e3c65b1fe5ea258fd302b694c7dbeec8cceec2
Priscilla <kayegipriscilla@gmail.com>
2023-03-01 17:57:14 +0300

    Implemented NextAuth authetication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,31 @@
+import spotifyApi, { LOGIN_URL } from "@/lib/spotify"
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+
+
+async function refreshAccessToken (){
+  try{
+    spotifyApi.setAccessToken(access.setAccessToken)
+    spotifyApi.setRefreshToken(access.setRefreshToken)
+
+    const {body: refreshedToken} = await spotifyApi.refreshAccessToken(token)
+    console.log('The refreshed token is', refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_at * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+      // Replace if new one came back if not Fall back to old refresh token 
+    }
+  }catch (error){
+    console.log(error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,6 +37,38 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        }
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.user.accessToken
+      session.user.refreshToken = token.user.refreshToken
+      session.user.email = token.user.email
+
+      return session
+    },
+  }
 }
 
 export default NextAuth(authOptions)
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { getProviders ,signIn } from 'next-auth/react';
 
-export default function login() {
+export default function login({providers}) {
   return (
-    <div>login</div>
+    <div className='flex flex-col min-h-screen items-center bg-black w-full justify-center'>
+      <img className='w-52 mb-5' src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg" alt="" />
+      {
+        providers && !!Object.keys(providers).length && Object.values(providers).map((provider) =>(
+          <div key = {provider.name}>
+            <button className='bg-[#18D860] text-white p-5 rounded-full'>Login with {provider.name}</button>
+          </div>
+        ))
+      }
+    </div>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  const providers = await getProviders(context);
+  return {
+    props: {
+      providers
+    }, // will be passed to the page component as props
+  }
 }
